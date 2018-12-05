82320e199a98e4eb68243073f8215d913b31ecde
Kim Andreasen <45560050+PilleStat@users.noreply.github.com>
2018-12-05 10:57:29 +0100

    Add files via upload

---
+++ PilleStat_aktiv_Version_V1/omregn_bit_xy.ino
@@ -0,0 +1,4 @@
+int omregn_bit_xy (byte val_x, byte val_y)
+  {
+    return ( val_x * 256 ) + val_y;
+  }
---
+++ PilleStat_aktiv_Version_V1/send_serial.ino
@@ -0,0 +1,15 @@
+void serial_send ()
+{
+
+    String Serial_Send_Data = "tid=";
+    Serial_Send_Data += Modul_ID;
+    for (int i = 0; i<=40; i++)
+      {
+        Serial_Send_Data += ",";
+        Serial_Send_Data += packetBuffer[i];
+      }
+      
+Serial.println (Serial_Send_Data);
+
+
+}
