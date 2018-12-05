b4ed31197f9098ea405e0d3f6d69aff74d4f298d
Kim Andreasen <45560050+PilleStat@users.noreply.github.com>
2018-12-05 11:55:23 +0100

    Delete send_udp.ino

--- PilleStat_aktiv_Version_V1/send_udp.ino
+++
@@ -1,7 +0,0 @@
-void udp_send ()
-{
-   
-  Udp.beginPacket(fjernserver, RemotePort);                     //   
-  Udp.write(packetBuffer, 70);                                  //
-  Udp.endPacket();                                              //
-}
